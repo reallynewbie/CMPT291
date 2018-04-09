708fb9bbe6966b39c9483c3d98eca442d9421425
Blahax <blehax11745@gmail.com>
2018-04-08 22:22:09 -0600

    Revert "Revert "Added Cust Rep Pages/Functions""
    This reverts commit ccbc868a0fbf7f53f42446f17fdb16e5fc8b8334.

---
+++ 291 Project/AddCustForm.vb
@@ -0,0 +1,13 @@
+﻿Public Class AddCustForm
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Label9_Click(sender As Object, e As EventArgs) Handles Label9.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
